31318a071e51c99469e3a9c6712f365cbb5d9b8d
Gioele Cantoni <gioele.cantoni@gmail.com>
2025-07-31 18:25:00 +0200

    bump version

--- src/sqlite-ai.h
+++ src/sqlite-ai.h
@@ -24,7 +24,7 @@
 extern "C" {
 #endif
 
-#define SQLITE_AI_VERSION "0.5.4"
+#define SQLITE_AI_VERSION "0.5.5"
 
 SQLITE_AI_API int sqlite3_ai_init (sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pApi);
 
