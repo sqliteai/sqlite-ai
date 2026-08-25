2ac0513b9c112b89641ec1972928c4184a4d7157
andinux <andinux@users.noreply.github.com>
2026-08-25 13:11:55 +0000

    Update Package.swift checksum for 1.0.7 [auto-update]

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "aiBinary",
-            url: "https://github.com/sqliteai/sqlite-ai/releases/download/1.0.6/ai-apple-xcframework-1.0.6.zip",
-            checksum: "64ecede326e1fb61f9cf47acea77f4aea5bef8b5ddd67aed40fa2ca184f604d7"
+            url: "https://github.com/sqliteai/sqlite-ai/releases/download/1.0.7/ai-apple-xcframework-1.0.7.zip",
+            checksum: "da1baecab99ea9f5f2ffd43d6b7965dc04e65ba6d37f9bcf51ef151c1176be67"
         ),
         .target(
             name: "ai",
